e007d34e656e9487736d04f3092cdf8463c94cbc
bigdatawyf <18665346@qq.com>
2019-05-31 16:04:53 +0800

    v1.2.1

--- src/main/java/com/atguigu/HelloWorld.java
+++ src/main/java/com/atguigu/HelloWorld.java
@@ -1,7 +1,6 @@
 package com.atguigu;
 
-public class HelloWorld
-{
+public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println("hello");
