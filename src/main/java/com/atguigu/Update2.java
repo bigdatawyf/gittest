76c7def4970df8cfd4b7df2b2b9ae49496d4d3fa
wyf <18665346@qq.com>
2019-05-31 16:43:39 +0800

    v1.2.2

--- src/main/java/com/atguigu/Update2.java
+++ src/main/java/com/atguigu/Update2.java
@@ -1,7 +1,12 @@
 package com.atguigu;
 
+import java.awt.*;
+
 public class Update2 {
     public static void main(String[] args) {
         System.out.println("hehe");
+
+        SystemColor;
+
     }
 }
